d4c6a1044fdc5fdb51ab4c7beebf9ebe96f05e40
hbkm12 <hbkm12@gmail.com>
2021-08-21 15:05:51 +0900

    build_제외

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/cpp_class_test
-# Build directory: D:/cpp_class_test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
